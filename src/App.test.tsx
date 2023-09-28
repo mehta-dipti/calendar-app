82869f16c4659ea0b3e9e27c28f9aa8bcdd836c1
dipti.mehta <dipti.mehta@globallogic.com>
2023-09-28 20:10:24 +0530

    updated basic tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders options of monthly and yearly calender", () => {
   render(<App />);
   const monthly = screen.getByText(/Monthly calendar/i);
   const yearly = screen.getByText(/Yearly calendar/i);
